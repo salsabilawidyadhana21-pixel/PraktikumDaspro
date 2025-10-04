52ddebd2fc12b0931f32a1ba14c94bca1e35d24e
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-10-04 11:49:32 +0700

    modif percobaan 1 (new)

--- jobsheet5/ifCetakKRS1.25.java
+++ jobsheet5/ifCetakKRS1.25.java
@@ -13,10 +13,12 @@ public static void main(String[] args) {
      if (uktLunas) {
           System.out.println("Pembayaran UKT terverifikasi");
           System.out.println("silahkan cetak KRS dan minta tanda tangan DPA");
-
+     }else{
+        System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
      }
+} 
 
 }
-}
+
     
 
