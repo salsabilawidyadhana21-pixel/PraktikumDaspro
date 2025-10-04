e13813c108984e76cb7408a1b5acb4aaac235fb3
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-10-04 10:01:44 +0700

    Tugas 3 sistem perpus 25

---
+++ jobsheet5/SistemPerpus25.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class SistemPerpus25 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+         System.out.print("Apakah kamu punya KTM? (true/false): ");
+         boolean ktm = sc.nextBoolean();
+         System.out.print("Apakah kamu sudah registrasi online? (true/false): ");
+         boolean regisOnline = sc.nextBoolean();
+
+         if (ktm || regisOnline) {
+            System.out.println("boleh masuk");
+         }else{
+            System.out.println("Maaf, kamu tidak boleh masuk");
+         }
+  
+    }
+}
+
+    
+
