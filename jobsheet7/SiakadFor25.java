723d3235825188e5877d72e0c650643946d6ef82
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-10-23 11:49:33 +0700

    percobaan 1

---
+++ jobsheet7/SiakadFor25.java
@@ -0,0 +1,26 @@
+ import java.util.Scanner;
+
+public class SiakadFor25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double nilai, tertinggi = 0;   // Inisialisasi tertinggi dengan 0
+        double terendah = 100;  // Inisialisasi terendah dengan 100
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        } // Akhir perulangan FOR
+        System.out.println("\nNilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+
+        sc.close();
+    }
+}
