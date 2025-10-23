101f277265645575f9ec67aabea4ce6dbe51709d
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-10-23 12:07:09 +0700

    modifikasi percobaan 2 (yg benar )

--- jobsheet7/SiakadWhile25.java
+++ jobsheet7/SiakadWhile25.java
@@ -21,7 +21,7 @@ public class SiakadWhile25 {
             // Cek Nilai Tidak Valid (negatif atau lebih dari 100)
             if (nilai < 0 || nilai > 100) { 
                 System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
-                continue; 
+                continue;
             }
 
             String kategoriHuruf = "";
@@ -29,23 +29,29 @@ public class SiakadWhile25 {
             // Kondisi pemilihan IF-ELSE IF-ELSE untuk menentukan kategori nilai huruf
             if (nilai > 80 && nilai <= 100) {
                 kategoriHuruf = "A";
+                // MODIFIKASI: Menampilkan pesan tambahan untuk nilai A
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 kategoriHuruf = "B+";
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
                 kategoriHuruf = "B";
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
             } else if (nilai > 60 && nilai <= 65) {
                 kategoriHuruf = "C+";
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
             } else if (nilai > 50 && nilai <= 60) {
                 kategoriHuruf = "C";
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
             } else if (nilai > 39 && nilai <= 50) {
                 kategoriHuruf = "D";
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
             } else { // nilai <= 39
                 kategoriHuruf = "E";
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
             }
             
-            // Output Kategori
-            System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah " + kategoriHuruf);
-
             i++; 
         } 
 
