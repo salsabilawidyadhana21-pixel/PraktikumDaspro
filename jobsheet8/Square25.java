dd95264ba0bfa679aa1dc122c5b61afc073da223
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-10-29 15:54:12 +0700

    modifikasi percobaan 2

--- jobsheet8/Square25.java
+++ jobsheet8/Square25.java
@@ -12,7 +12,7 @@ public class Square25 {
         // Outer Loop (iOuter=1)
         for (int iOuter = 1; iOuter <= N; iOuter++) { 
             
-            // Inner Loop (i=0): Mencetak Bintang per Baris (N+1 kali)
+            // Inner loop (i=0): mencetak bintang per Baris (N+1 kali)
             // i=0 dan i<=N membuat perulangan berjalan untuk i=0, 1, ..., N (N+1 iterasi)
             for(int i = 0; i <= N; i++) { 
                 System.out.print("*"); // Mencetak '*' tanpa baris baru
