905078acaefc6a318e4a3c07720306a5e54a2b56
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-10-29 15:50:37 +0700

    modifikasi percobaan 2

--- jobsheet8/Square25.java
+++ jobsheet8/Square25.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 
 public class Square25 {
     public static void main(String[] args) {
-        //Inisialisasi Scanner dan Input N
+        // Deklarasi objek Scanner
         Scanner sc = new Scanner(System.in); 
-        System.out.print("Masukkan nilai n="); 
-        int n = sc.nextInt(); 
+        
+        // Input nilai N
+        System.out.print("Masukkan nilai N: "); 
+        int N = sc.nextInt(); 
 
-        //Outer Loop 
-        for (int iOuter = 1; iOuter <= n; iOuter++) { 
-
-            //Inner Loop 
-            for(int i = 1; i <= n; i++) { 
-                System.out.print("*"); 
+        // Outer Loop (iOuter=1)
+        for (int iOuter = 1; iOuter <= N; iOuter++) { 
+            
+            // Inner Loop (i=0): Mencetak Bintang per Baris (N+1 kali)
+            // i=0 dan i<=N membuat perulangan berjalan untuk i=0, 1, ..., N (N+1 iterasi)
+            for(int i = 0; i <= N; i++) { 
+                System.out.print("*"); // Mencetak '*' tanpa baris baru
             }
 
             System.out.println(); 
