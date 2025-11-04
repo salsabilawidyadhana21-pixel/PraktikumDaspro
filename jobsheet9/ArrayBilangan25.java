6514b6a4b38666af97be5fdf57338f31ce26e17a
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-11-04 10:48:09 +0700

    percobaan 1

---
+++ jobsheet9/ArrayBilangan25.java
@@ -0,0 +1,17 @@
+public class ArrayBilangan25 { 
+    public static void main(String[] args) {
+        
+        int[] bil = new int[4]; 
+
+        // Isi masing-masing elemen array bil dengan angka 5, 13, -7, 17.
+        bil[0] = 5;    
+        bil[1] = 13;   
+        bil[2] = -7;   
+        bil[3] = 17;   
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]); 
+        System.out.println(bil[2]);
+        System.out.println(bil[3]); 
+    }
+}
