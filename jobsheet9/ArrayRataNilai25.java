0b348b8172f34dcf8561e599f749a3f96e67e8d3
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-11-05 08:52:00 +0700

    modifikasi percobaan 3 1.1

--- jobsheet9/ArrayRataNilai25.java
+++ jobsheet9/ArrayRataNilai25.java
@@ -2,31 +2,34 @@ import java.util.Scanner;
 
 public class ArrayRataNilai25 { 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // Deklarasi Scanner
-
-        // Deklarasi array dan variabel
+        Scanner sc = new Scanner(System.in); 
         int[] nilaiMhs = new int[10];      
         double total = 0;                  
         double rata2;                      
+        int lulus = 0; // Tambahkan variabel untuk menghitung jumlah yang lulus
 
         // Isi array nilaiMhs dengan input pengguna
         for (int i = 0; i < nilaiMhs.length; i++) {
-            // Menggunakan (i+1) agar input dimulai dari "Mahasiswa ke-1"
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ":"); 
             nilaiMhs[i] = sc.nextInt();
         }
 
-        // Gunakan perulangan untuk menghitung jumlah keseluruhan nilai (total)
+        // Hitung total dan jumlah yang lulus
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i]; 
+            
+            // Untuk menghitung yang lulus
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+            }
         }
 
         // Hitung nilai rata-rata
-        // Menggunakan nilaiMhs.length sebagai pembagi (jumlah elemen)
         rata2 = total / nilaiMhs.length; 
         
-        // Tampilkan hasil rata-rata
         System.out.println("Rata-rata nilai = " + rata2);
+        // Tampilkan jumlah mahasiswa yang lulus
+        System.out.println("Banyaknya mahasiswa yang lulus adalah: " + lulus);
 
         sc.close();
     }
