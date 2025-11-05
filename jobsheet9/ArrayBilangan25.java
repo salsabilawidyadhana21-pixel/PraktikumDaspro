485237954ecd1b2f1307b7e9638dbf008f72bdd2
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-11-05 08:22:12 +0700

    modifikasi percobaan 1

--- jobsheet9/ArrayBilangan25.java
+++ jobsheet9/ArrayBilangan25.java
@@ -1,17 +1,11 @@
-public class ArrayBilangan25 { 
+public class ArrayBilangan25 {
     public static void main(String[] args) {
-        
-        int[] bil = new int[4]; 
-
-        // Isi masing-masing elemen array bil dengan angka 5, 13, -7, 17.
-        bil[0] = 5;    
-        bil[1] = 13;   
-        bil[2] = -7;   
-        bil[3] = 17;   
+        // Modifikasi: Inisialisasi elemen array sekaligus pada saat deklarasi
+        int[] bil = {5, 13, -7, 17}; 
 
         System.out.println(bil[0]);
-        System.out.println(bil[1]); 
+        System.out.println(bil[1]);
         System.out.println(bil[2]);
-        System.out.println(bil[3]); 
+        System.out.println(bil[3]);
     }
 }
