a72dbdfbe0d198a84d13d073607932fa866b17bb
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-11-05 09:24:57 +0700

    modifikasi percobaan 4 1.2

--- jobsheet9/SearchNilai25.java
+++ jobsheet9/SearchNilai25.java
@@ -5,23 +5,23 @@ public class SearchNilai25 {
         Scanner sc = new Scanner(System.in); 
         
         // Input banyaknya elemen array
-        System.out.print("Masukkan banyaknya nilai yang akan diinput: "); 
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
         int jmlElemen = sc.nextInt();
         
         // Deklarasi array sesuai input jumlah elemen
         int[] arrNilai = new int[jmlElemen];
         
-        // Inisialisasi hasil dengan -1. Digunakan sebagai indikator 'tidak ditemukan'.
+        // Inisialisasi hasil dengan -1. Nilai ini menandakan 'key' belum ditemukan.
         int hasil = -1; 
 
         // Input isi array
         for (int i = 0; i < arrNilai.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ":\n"); // Menggunakan i+1 agar dimulai dari 1
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             arrNilai[i] = sc.nextInt();
         }
 
         // Input nilai (key) yang ingin dicari
-        System.out.print("Masukkan nilai yang ingin dicari: "); 
+        System.out.print("Masukkan nilai yang ingin dicari: ");
         int key = sc.nextInt();
 
         // Proses linear search
@@ -32,15 +32,16 @@ public class SearchNilai25 {
             }
         }
 
-        // Output hasil pencarian
+        // Output hasil pencarian (Sesuai Pertanyaan 3)
+        System.out.println(); 
         if (hasil != -1) {
-            // Posisi mahasiswa = indeks + 1 (karena user melihatnya mulai dari 1)
-            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1)); 
+            // Jika nilai ditemukan (hasil bukan -1)
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
         } else {
-            // Menangani kasus jika nilai tidak ditemukan (sesuai Pertanyaan 3)
-            System.out.println("Nilai yang dicari tidak ditemukan"); 
+            // Jika nilai tidak ditemukan (hasil tetap -1)
+            System.out.println("Nilai yang dicari tidak ditemukan");
         }
         
         sc.close();
-    }
-}
+    } 
+} 
