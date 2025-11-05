d892671af9aecbc7ff7fefb50dd86f15a6d3bb95
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-11-05 08:42:22 +0700

    modifikasi percobaan 2

--- jobsheet9/ArrayNilai25.java
+++ jobsheet9/ArrayNilai25.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class ArrayNilai25 { 
     public static void main(String[] args) {
-        // Deklarasi Scanner untuk input
         Scanner sc = new Scanner(System.in); 
 
-        // Deklarasi array nilaiAkhir dengan kapasitas 10
-        int[] nilaiAkhir = new int[10];
+        // Deklarasi array nilaiAkhir dengan kapasitas 10 elemen
+        int[] nilaiAkhir = new int[10]; 
 
         // Perulangan untuk menerima input dan mengisi elemen array
         for (int i = 0; i < nilaiAkhir.length; i++) {
@@ -14,11 +13,19 @@ public class ArrayNilai25 {
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        // Perulangan untuk menampilkan semua isi elemen array
-        System.out.println("\nHasil Input:");
-        for (int i = 0; i < nilaiAkhir.length; i++) {
-            if (nilaiAkhir[i] > 70)
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        // Modifikasi (Pertanyaan 4): Menampilkan status kelulusan semua mahasiswa
+        System.out.println("\n===== Hasil Status Kelulusan =====");
+        
+        // Perulangan untuk memeriksa setiap elemen array
+        for (int i = 0; i < nilaiAkhir.length; i++) { 
+            // Pengecekan kondisi: apakah nilai lebih besar dari 70?
+            if (nilaiAkhir[i] > 70) {
+                // Jika BENAR (Lulus)
+                System.out.println("Mahasiswa ke-" + i + " lulus!"); 
+            } else {
+                // Jika SALAH (Tidak Lulus, nilai <= 70)
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!"); 
+            }
         }
         
         sc.close();
