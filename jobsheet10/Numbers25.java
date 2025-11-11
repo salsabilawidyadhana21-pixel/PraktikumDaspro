fd31b8cd7a34049d23f965c5d86119e592e0191a
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-11-11 14:10:18 +0700

    Percobaan 3

---
+++ jobsheet10/Numbers25.java
@@ -0,0 +1,20 @@
+public class Numbers25 { 
+    public static void main(String[] args) {
+        
+        // Deklarasi array 2 dimensi bertipe int dengan 3 baris
+        int[][] myNumbers = new int[3][];
+        
+        // Instansiasi setiap baris dengan panjang yang berbeda
+        // Baris ke-1 (indeks 0) memiliki 5 kolom
+        myNumbers[0] = new int[5]; 
+        
+        // Baris ke-2 (indeks 1) memiliki 3 kolom
+        myNumbers[1] = new int[3]; 
+        
+        // Baris ke-3 (indeks 2) memiliki 1 kolom
+        myNumbers[2] = new int[1]; 
+        
+        // Program selesai. 
+        
+    }
+}
