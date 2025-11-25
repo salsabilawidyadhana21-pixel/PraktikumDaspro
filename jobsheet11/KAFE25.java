23825a3d9b8168e50bf160984a7dd500d804e096
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-11-25 16:53:57 +0700

    Modifikasi Percobaan 1

--- jobsheet11/KAFE25.java
+++ jobsheet11/KAFE25.java
@@ -1,19 +1,15 @@
 public class KAFE25 {
-
-    public static void Menu() {
-        System.out.println("====== MENU RESTO KAFE ======"); 
-        System.out.println("1. Kopi Hitam\t\tRp 15,000"); 
-        System.out.println("2. Cappuccino\t\tRp 20,000");
-        System.out.println("3. Latte\t\tRp 22,000"); 
-        System.out.println("4. Teh Tarik\t\tRp 12,000"); 
-        System.out.println("5. Roti Bakar\t\tRp 10,000"); 
-        System.out.println("6. Mie Goreng\t\tRp 18,000"); 
-        System.out.println("============================="); 
-        System.out.println("Silakan pilih menu yang Anda inginkan."); 
-    }
-
-   
+    
     public static void main(String[] args) {
-        Menu(); // Memanggil fungsi Menu()
+        // Daftar menu ditampilkan langsung di dalam fungsi main.
+        System.out.println("====== MENU RESTO KAFE ======"); 
+        System.out.println("1. Kopi Hitam\t\tRp 15,000");
+        System.out.println("2. Cappuccino\t\tRp 20,000");
+        System.out.println("3. Latte\t\tRp 22,000");
+        System.out.println("4. Teh Tarik\t\tRp 12,000");
+        System.out.println("5. Roti Bakar\t\tRp 10,000");
+        System.out.println("6. Mie Goreng\t\tRp 18,000");
+        System.out.println("=============================");
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
 }
