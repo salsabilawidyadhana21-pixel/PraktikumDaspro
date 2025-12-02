bd9a2d276eaac429736ed8a7a169f88d77847239
salsabilawidyadhana21-pixel <salsabilawidyadhana21@gmail.com>
2025-12-02 18:51:41 +0700

    Modifikasi percobaan 2 (25)

--- jobsheet12/Percobaan2.java
+++ jobsheet12/Percobaan2.java
@@ -4,37 +4,45 @@ public class Percobaan2 {
 
     /**
      * Fungsi rekursif untuk menghitung pangkat (x^y).
-     * @param x Bilangan yang akan dihitung pangkatnya.
+     * Fungsi ini dimodifikasi untuk mencetak deret perkalian saat melakukan ekspansi.
+     * @param x Bilangan yang akan dihitung pangkatnya (Bilangan Dasar).
      * @param y Bilangan pangkatnya.
      * @return Hasil dari x^y.
      */
     static int hitungPangkat(int x, int y) {
-        // Base case: Jika pangkat y = 0, kembalikan 1 (karena x^0 = 1)
+        // Base case: Jika pangkat y = 0.
         if (y == 0) {
-            return (1);
+            // Modifikasi: Cetak '1' sebagai faktor terakhir.
+            System.out.print("1"); 
+            return 1;
         } else {
-            return (x * hitungPangkat(x, y - 1)); 
+            // Modifikasi: Cetak bilangan dasar (x) dan operator perkalian 'x'.
+            System.out.print(x + "x"); 
+            
+            // pemanggilan rekursif
+            return x * hitungPangkat(x, y - 1); 
         }
     }
 
     public static void main(String[] args) {
         // Deklarasi Scanner untuk input
-        Scanner sc = new Scanner(System.in); //
+        Scanner sc = new Scanner(System.in); 
 
-        // Deklarasi variabel
-        int bilangan; //
-        int pangkat; //
+        // Input
+        System.out.print("Bilangan yang dihitung: ");
+        int bilangan = sc.nextInt();
+        System.out.print("Pangkat: ");
+        int pangkat = sc.nextInt();
 
-        // Menerima input dari keyboard
-        System.out.print("Bilangan yang dihitung: "); //
-        bilangan = sc.nextInt(); //
-        System.out.print("Pangkat: "); //
-        pangkat = sc.nextInt(); //
+        // menampilkan prompt perhitungan sebelum pemanggilan rekursif
+        System.out.print(bilangan + "^" + pangkat + " = ");
 
-        // Memanggil fungsi hitungPangkat dan menampilkan hasilnya
-        System.out.println("Hasil " + bilangan + "^" + pangkat + ": " + hitungPangkat(bilangan, pangkat)); //
+        // memanggil fungsi yang akan mencetak deret dan menghitung hasilnya
+        int hasil = hitungPangkat(bilangan, pangkat);
+
+        // menampilkan hasil akhir
+        System.out.println(" = " + hasil);
         
-        // Tutup scanner
         sc.close();
     }
 }
